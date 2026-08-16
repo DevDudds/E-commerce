272d9e1fb8f5179f3ba9b00e182138d5da5f4805
Dudds <duddscosta@gmail.com>
2026-08-15 22:07:01 -0300

    feat(G-5): Adiciona botão para a pagina de produtos no header

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -32,7 +32,7 @@ export async function carregarProdutos() {
                 <div class="card-produto">
 
                     <div class="produto-imagem">
-                        <img class="card-produto-img" src="./assets/imgs/4080.jpg" alt="">
+                        <img class="card-produto-img" src="${produto.imagem}" alt="">
                         <div class="etiquetas">
                             <span class="desconto">-${(produto.desconto*100).toFixed(0)}%</span>
                         </div>
